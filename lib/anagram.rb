25654a7fdfba35af75b1628b22f67936960c7267
Tracy Reuther <tracyreuther@gmail.com>
2019-06-25 22:35:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize(word)
 end
 
 def match(word_array)
-  word_array.each do |ea_word|
+  word_array.select do |ea_word|
     ea_word.split("")
     if ea_word.sort == @word.sort
       word_array
